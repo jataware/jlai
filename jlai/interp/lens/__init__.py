6ca7329a215eeb76162b3240739568bf0bc045f3
bkj <bkj.322@gmail.com>
2025-09-08 20:18:34 -0400

    lens

--- jlai/interp/lens/__init__.py
+++ jlai/interp/lens/__init__.py
@@ -39,12 +39,12 @@ async def main():
         pattern = re.compile(r'blocks\.(\d+)\.hook_resid_post')
         match   = pattern.match(layer_name)
         if match:
-            return int(match.group(1)) >= 25
+            return int(match.group(1)) >= 20
         
         return False
     
     hook_names = client.model.hook_names.remote()
-    hook_names = set([name for name in hook_names if names_filter(name)])
+    hook_names = [name for name in hook_names if names_filter(name)]
     rprint(hook_names)
 
     async for result in client.abatched_forward(messages, tokens_per_batch=51, return_logits=False, names_filter=hook_names):
